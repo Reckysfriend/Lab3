5a7b7df2dd4286455137b68d45742fd8d330dd7b
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-12-09 08:42:56 +0100

    Added assignment tasks

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,5 +1,19 @@
 ﻿namespace Lab3
 {
+/*
+ * Static Array with all dates of May
+ * A class for temperature
+ * A way to display all the temperature of all days in May
+ * A way to display the average emperature in May based on array.
+ * A way to find the hottest day in May and display it.
+ * A way to find the coldest day in May and display it.
+ * A way to find the median temperature of May and display it.
+ * A way to sort the temperature both ascending and descending.
+ * A way to only show days above X temperature to identifiy warmer days.
+ * Ability to enter a date and return the temperature of that day and its neighbouring days.
+ * A way to get the most common temperature in May.
+ * Fill the May-array with random elements via Random
+ */
     internal class Program
     {
         static void Main(string[] args)
