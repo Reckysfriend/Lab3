2b579555a14ffe095b3beb1217f47d3213bad9fb
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-12-10 13:37:19 +0100

    Added checks
    Added basic checks to see that our program run as intended so far.

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -16,34 +16,52 @@ namespace Lab3
  * A way to get the most common temperature in May.
  * Fill the May-array with random elements via Random
  */
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
+            Temperature temperature = new Temperature();
+            Console.Write("Please enter any amount of days:");
+            Int32.TryParse(Console.ReadLine(), out int userInput);
+            int[] temperatureArray = temperature.RandomTemperaturesInMonth(userInput);
+            temperature.PrintArray(temperatureArray, "May");
+            Console.WriteLine(" ");
+            temperature.AverageTemperature(temperatureArray);
+            Console.ReadLine();
+
         }
     }
     public class Temperature
     {
-        static int[] RandomTemperaturesInMonth(int days)
+        public int[] RandomTemperaturesInMonth(int days)
         {
             int[] temperatureArray = new int[days];
             Random random = new Random();
-            return x;
             for (int i = 0; i < temperatureArray.Length; i++)
             {
                 int x = random.Next(0, 25 + 1);
                 temperatureArray[i] = x;
             }
-            
+            return temperatureArray;
         }
-        static void PrintArray(int[] intArray)
+         public void PrintArray(int[] intArray , string month)
         {
             int y = 0;
             foreach (int x in intArray)
             {
                 y++;
-                Console.WriteLine($"May {y}: {x}°C");
+                Console.WriteLine($"{month} {y}: {x}°C");
             }
+        }
+        public void AverageTemperature(int[] intArray)
+        {
+            int sumOfArray = 0;
+            foreach (int x in intArray)
+            {
+                sumOfArray += x;
+            }
+            int averageSumOfArray = sumOfArray / intArray.Length;
+            Console.WriteLine($"The average temperature of the month is {averageSumOfArray}!");
         }
     }
 }
